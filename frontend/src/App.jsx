d1dceb6b7371e06ba2ccad832ad490ba2df8a970
eniszgr <zgr.enis@gmail.com>
2025-05-28 10:20:14 +0300

    fonts added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import WelcomePage from './components/WelcomePage';
+import React from 'react'
 
 function App() {
-
-
   return (
-    <div className='flex h-screen w-screen bg-gray-100'>
-     <WelcomePage/>
+    <div className='p-4 m-12'>
+      <div className='container bg-red-500 text-white p-4 mg-auto'>
+        <h1 className='text-8xl'>tailwindcss is working!</h1>
+        <h2 className='text-3xl'>text3xl</h2>
+        <h2 className='text-3xl font-cherry'>text3xl</h2>
+        <h2 className='text-3xl font-ikinci'>text3xl</h2>
+        <h2 className='text-3xl font-ikinci testcss'>text3xl</h2>
+      </div>
     </div>
   )
 }
